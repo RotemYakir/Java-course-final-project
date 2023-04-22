f0b6324ffa2025a851f1fff2b7544ab304c12533
ryaki <ryaki@LAPTOP-ESGLLBM2.lan>
2023-04-22 16:15:43 +0300

    project

--- Final-Project-Phase-3-Rotem-Yakir/src/main/java/app/core/auth/AuthController.java
+++ Final-Project-Phase-3-Rotem-Yakir/src/main/java/app/core/auth/AuthController.java
@@ -1,5 +1,42 @@
 package app.core.auth;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/login")
+@CrossOrigin
 public class AuthController {
+
+//	@Autowired
+//	AdminService adminService;
+//	@Autowired
+//	CompanyService companyService;
+//	@Autowired
+//	CustomerService customerService;
+//
+//	@PostMapping("/admin")
+//	public String adminLogin(@RequestBody UserCredentials credentials) {
+//		try {
+//			return adminService.login(credentials);
+//		} catch (CouponSystemException e) {
+//			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+//		}
+//	}
+//	@PostMapping("/company")
+//	public String companyLogin(@RequestBody UserCredentials credentials) {
+//		try {
+//			return companyService.login(credentials);
+//		} catch (CouponSystemException e) {
+//			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+//		}
+//	}
+//	@PostMapping("/customer")
+//	public String customerLogin(@RequestBody UserCredentials credentials) {
+//		try {
+//			return customerService.login(credentials);
+//		} catch (CouponSystemException e) {
+//			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+//		}
+//	}
 
 }
--- Final-Project-Phase-3-Rotem-Yakir/src/main/java/app/core/controllers/CustomerController.java
+++ Final-Project-Phase-3-Rotem-Yakir/src/main/java/app/core/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +23,7 @@ import app.core.services.CustomerService;
 
 @RestController
 @RequestMapping("/api/customer")
+@CrossOrigin
 public class CustomerController {
 
 	@Autowired
